ae9040f603fe61383e7b098dfca769b127bf625a
rh00n <142110851+rh00n@users.noreply.github.com>
2026-05-13 10:54:16 +0100

    Initial Commit

---
+++ AMICA/AMICAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMICAApp.swift
+//  AMICA
+//
+//  Created by Rhoon Racoon on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AMICAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
